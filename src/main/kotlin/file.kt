1d70e90ed79583a1eadebe70734597a280af486a
EV-55 <vasiliev_egor_55@mail.ru>
2021-08-09 18:21:33 +0700

    all setup

--- src/main/kotlin/file.kt
+++ src/main/kotlin/file.kt
@@ -1,35 +1,68 @@
 fun main() {
-var x = arrayOf(1,1,1,1,1,1,1,2,2,2,2,2)
-    var y = arrayOf(3,2,2)
+
+    var x = arrayListOf<Int>(1, 1, 1, 2, 2, 2, 3)
+
+    var iterator = x.iterator()
+    var masst = arrayListOf<Pair<Int, ArrayList<Int>>>()
+
+    var current = 99//x[0]
+    var last = current
+
+    while (iterator.hasNext()) {
+        current = iterator.next()
+
+        if (current == last) {
+            masst[masst.lastIndex].first
+        } else {
+            masst.add(1)
+        }
+        last = current
+    }
+    println(masst)
 
 
-
-    //    if (1 == x[0] ){
-//        if(1 == x[1]){
-//            if(1 == x[2]){
-//                println(123123)
-//            }
+//    for(i in iterator) {
+//        var first = x[0]
+//        schetchik += i
+//        iterator.hasNext()
+//        while (schetchik == first) {
+//            t++
+//
 //        }
-//    }
-//    if(x[0] == x[1] && x[0] == x[2]){
-//        println(123123123123123)
-//    }
-fun chekRow(x : Array<Int>)  {
-        var f2 = x.count { it == 2 }
-        var f3 = x.count{ it == 3}
-        var f1 = x.count { it == 1 }
-if (f2 >= 3){
-    println(2)
+//        while (schetchik != first ) {
+//            t++
+//
+//        }
+
+//        while(first == i){
+//
+//
 }
-        if(f3 >= 3){
-            println(3)
-        }
-        if (f1 >= 3){
-            println(1)
-        }
+
+
+/*println(schetchik)
+    println(t)*/
+
+
+
+
+
+fun chekRow(x: Array<Int>) {
+    var f2 = x.count { it == 2 }
+    var f3 = x.count { it == 3 }
+    var f1 = x.count { it == 1 }
+    if (f2 >= 3) {
+        println(2)
+    }
+    if (f3 >= 3) {
+        println(3)
+    }
+    if (f1 >= 3) {
+        println(1)
+    }
 
 }
-chekRow(x)
 
-}
+
+
 
